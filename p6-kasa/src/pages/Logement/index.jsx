236b28967289b0fe70682ede52e388900173f385
Gwladys MORLIER <gwladys.morlier@hotmail.fr>
2023-08-15 19:42:06 -0400

    creation page logement, ajout des scss

--- p6-kasa/src/pages/Logement/index.jsx
+++ p6-kasa/src/pages/Logement/index.jsx
@@ -1,33 +1,37 @@
-function Logement({ title, pictures, description, name, picture, rating, location, equipments, tags }) {
+import './logement.scss'
+
+function Logement() {
     return (
         <div className="logement">
-            <img src={pictures} alt="" />
-            <div className="logement__title">
-                <h2>{title}</h2>
-                <h3>{location}</h3>
-            </div>
-            <div className="logement__host">
-                <p>{name}</p>
-                <img src={picture} alt="" />
+            <img src="./assets/Image-source-1.png" alt="" />
+            <div className='title__host'>
+                <div className="logement__title">
+                    <h2>Cozy loft on the Canal Saint-Martin</h2>
+                    <h3>Location</h3>
+                </div>
+                <div className="logement__host">
+                    <p>First name Last name</p>
+                    <img src="./assets/Image-source-1.png" alt="" />
+                </div>
             </div>
             <div className="logement__divers">
-                <p>{tags}</p>
-                <p>{rating}</p>
+                <p className='logement__divers--tags'>Tags</p>
+                <p>rating</p>
             </div>
             <div className="logement__informations">
                 <div className="logement__description">
-                    <div>
+                    <div className='logement__informations--title'>
                         <h2>Description</h2>
                         <button><img src="./assets/Vector.png" alt="Afficher les détails" /></button>
                     </div>
-                    <p>{description}</p>
+                    <p className='logement__text'>ecrire la description</p>
                 </div>
                 <div className="logement__equipements">
-                    <div>
+                    <div className='logement__informations--title'>
                         <h2>Equipements</h2>
                         <button><img src="./assets/Vector.png" alt="Afficher les détails" /></button>
                     </div>
-                    <p>{equipments}</p>
+                    <p className='logement__text'>Lister les équipements</p>
                 </div>
             </div>
         </div>
